6e6e67e467b710207e850a1692eca83832984857
Eric Pham <ericpham380@gmail.com>
2016-04-20 12:14:24 -0700

    Built scroll on buttons

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   
+  /* Scroll for navigation */
   $('.js--section-features').waypoint(function(direction){
     if(direction == "down"){
       $('nav').addClass('sticky');
@@ -8,7 +9,12 @@ $(document).ready(function() {
     }
   }, { offset: '60px;'});
   
+  /* Scroll on button */
+  $('.js--scroll-to-plans').click(function(){ 
+    $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+  });
   
-  
-  
+  $('.js--scroll-to-start').click(function(){ 
+    $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+  });
 });
