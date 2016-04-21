7247c78bbb47330f521e71098db5dbdc2db240a4
Eric Pham <ericpham380@gmail.com>
2016-04-21 14:33:13 -0700

    Made the navigation responsive

--- resources/js/script.js
+++ resources/js/script.js
@@ -46,23 +46,39 @@ $(document).ready(function () {
   }, {
     offset: '50%'
   });
-  
+
   $('.js--wp-2').waypoint(function (direction) {
     $('.js--wp-2').addClass('animated fadeInUp');
   }, {
     offset: '50%'
   });
-  
+
   $('.js--wp-3').waypoint(function (direction) {
     $('.js--wp-3').addClass('animated fadeIn');
   }, {
     offset: '50%'
   });
-  
+
   $('.js--wp-4').waypoint(function (direction) {
     $('.js--wp-4').addClass('animated bounce');
   }, {
     offset: '50%'
+  });
+
+  /* Responsive navigation */
+  $('.js--mobile-nav').click(function () {
+    var nav = $('.js--main-nav');
+    var icon = $('.js--mobile-nav i');
+
+    nav.slideToggle(200);
+
+    if (icon.hasClass('ion-navicon-round')) {
+      icon.addClass('ion-close-round');
+      icon.removeClass('ion-navicon-round');
+    } else {
+      icon.addClass('ion-navicon-round');
+      icon.removeClass('ion-close-round');
+    }
   });
 
 });
